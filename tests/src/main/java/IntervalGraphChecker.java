13fc344a3c8d07996e6c2a4fc11979d06b81d023
Sofia Titkova <sdtitkova@stud.etu.ru>
2025-05-16 23:25:13 +0300

    Delete tests directory

--- tests/src/main/java/IntervalGraphChecker.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-
-public class IntervalGraphChecker {
-    public boolean isIntervalGraph(Map<String, List<String>> graph) {
-        // Заглушка: возвращаем false, пока код не написан
-        return false;
-    }
-}
